b14429a9cd5318bf3c7f16d5c718ff742ba1c68b
CyberBatMan2077 <liaozifan6332@qq.com>
2020-11-18 11:03:27 +0000

    Initial commit

---
+++ poundtoRMB.py
@@ -0,0 +1,15 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+
+def getnumber():
+	html = urlopen("https://www.baidu.com/s?ie=UTF-8&wd=%E8%8B%B1%E9%95%91%E6%B1%87%E7%8E%87")
+
+	bsObj = BeautifulSoup(html, "html.parser")
+	# tables = bsObj.findAll("script", {"data-compress": {"off"}})
+	data = bsObj.find("div", {"class": {"op_exrate_result"}})
+	number = re.findall(r"[1-9]+\.+[0-9]+", data.get_text())
+	print(data.get_text())
+	standard = float(number[0])
+	return standard
