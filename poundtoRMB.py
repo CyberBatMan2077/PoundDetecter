72fe63d89fc6c286c9891e844ec9653aeccb66c0
CyberBatMan2077 <liaozifan6332@qq.com>
2020-11-19 22:02:48 +0000

    add error exception

--- poundtoRMB.py
+++ poundtoRMB.py
@@ -9,7 +9,16 @@ def getnumber():
 	bsObj = BeautifulSoup(html, "html.parser")
 	# tables = bsObj.findAll("script", {"data-compress": {"off"}})
 	data = bsObj.find("div", {"class": {"op_exrate_result"}})
-	number = re.findall(r"[1-9]+\.+[0-9]+", data.get_text())
-	print(data.get_text())
-	standard = float(number[0])
+	try:
+		number = re.findall(r"[1-9]+\.+[0-9]+", data.get_text())
+
+	except AttributeError:
+		number = None
+
+	if number is not None:
+		print(data.get_text())
+		standard = float(number[0])
+	else:
+		standard = None
+
 	return standard
