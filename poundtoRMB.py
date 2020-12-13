62b4272632d371ec54b1aecc893c19eded2d6ad0
CyberBatMan2077 <liaozifan6332@qq.com>
2020-12-13 20:37:34 +0000

    add timeout solution for urlopen

--- poundtoRMB.py
+++ poundtoRMB.py
@@ -4,12 +4,11 @@ import re
 
 
 def getnumber():
-	html = urlopen("http://www.baidu.com/s?ie=UTF-8&wd=%E8%8B%B1%E9%95%91%E6%B1%87%E7%8E%87")
-
-	bsObj = BeautifulSoup(html, "html.parser")
-	# tables = bsObj.findAll("script", {"data-compress": {"off"}})
-	data = bsObj.find("div", {"class": {"op_exrate_result"}})
 	try:
+		html = urlopen("http://www.baidu.com/s?ie=UTF-8&wd=%E8%8B%B1%E9%95%91%E6%B1%87%E7%8E%87",timeout=60)
+		bsObj = BeautifulSoup(html, "html.parser")
+		# tables = bsObj.findAll("script", {"data-compress": {"off"}})
+		data = bsObj.find("div", {"class": {"op_exrate_result"}})
 		number = re.findall(r"[1-9]+\.+[0-9]+", data.get_text())
 
 	except AttributeError:
